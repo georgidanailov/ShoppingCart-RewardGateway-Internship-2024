07ab61aba70b4126253e993b48b074d9ef53ef27
Georgi Danailov <demonhandz@gmail.com>
2024-08-09 00:25:21 +0300

    Add to cart logic and Shopping Cart styling

--- frontend/script.js
+++ frontend/script.js
@@ -38,4 +38,33 @@ window.addEventListener('DOMContentLoaded', ()=>{
 
         })
     })
+    let spy = new MutationObserver(mutations=>{
+        mutations.forEach(mutation=>{
+            if(!mutation.addedNodes.length == 1) return
+            if(mutation.target.classList.contains('item-container')){
+                mutation.addedNodes[0].children[3].addEventListener('click', addCartElm)
+            }
+        })
+    })
+    spy.observe(itemContainer, {childList:true})
+
+    const tbody = document.querySelector('tbody');
+
+    function addCartElm(){
+        const tr = document.createElement('tr')
+        tr.classList.add('item-now')
+        tr.setAttribute('item-id', this.parentElement.getAttribute('item-id'))
+        tr.innerHTML = `
+        <td class="item-img"> <img src="${this.parentElement.children[0].src}"> </td>
+        <td> ${this.parentElement.children[1].innerText}</td>
+        <td> ${this.parentElement.children[2].innerText}</td>
+        <td><input type="number" value="1"></td>
+        <td><button class="rm-btn">Remove</td>
+        `;
+        tbody.insertBefore(tr, tbody.lastElementChild)
+        updateTotal()
+    }
+
+    function updateTotal(){}
+
 })
