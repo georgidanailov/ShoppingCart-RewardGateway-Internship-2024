ccbec7a669d2bfa92958e22843474f5366a5a6a7
Georgi Danailov <demonhandz@gmail.com>
2024-08-09 01:21:17 +0300

    Update Cart Total logic

--- frontend/script.js
+++ frontend/script.js
@@ -2,9 +2,9 @@ window.addEventListener('DOMContentLoaded', ()=>{
     const shopBtn = document.querySelector('.shopBtn')
     const cartBtn = document.querySelector('.cartBtn')
     const itemContainer = document.querySelector('.item-container')
-    
     const shopSec = document.querySelector('.shop')
     const cartSec = document.querySelector('.cart')
+    const tbody = document.querySelector('tbody');
     
     shopBtn.addEventListener('click', ()=>{
         cartSec.style.display="none"
@@ -18,7 +18,7 @@ window.addEventListener('DOMContentLoaded', ()=>{
 
     fetch('http://localhost:5500/backend/myserver.php', {
         method:'GET',
-        header:{
+        headers:{
             'Content-Type':'application/json',
             'Accept':'application/json'
         }
@@ -38,17 +38,21 @@ window.addEventListener('DOMContentLoaded', ()=>{
 
         })
     })
-    let spy = new MutationObserver(mutations=>{
+    let observer = new MutationObserver(mutations=>{
         mutations.forEach(mutation=>{
-            if(!mutation.addedNodes.length == 1) return
+            if(mutation.addedNodes.length !== 1) return
             if(mutation.target.classList.contains('item-container')){
                 mutation.addedNodes[0].children[3].addEventListener('click', addCartElm)
             }
+            if(mutation.target.classList.contains('tbody')){
+                mutation.addedNodes[0].lastElementChild.addEventListener('click',rmCartElm)
+                const quantity = mutation.addedNodes[0].children[3]
+                quantity.addEventListener('change', quantityChange)
+            }
         })
     })
-    spy.observe(itemContainer, {childList:true})
-
-    const tbody = document.querySelector('tbody');
+    observer.observe(itemContainer, {childList:true})
+    observer.observe(tbody, {childList:true})
 
     function addCartElm(){
         const tr = document.createElement('tr')
@@ -60,11 +64,37 @@ window.addEventListener('DOMContentLoaded', ()=>{
         <td> ${this.parentElement.children[2].innerText}</td>
         <td><input type="number" value="1"></td>
         <td><button class="rm-btn">Remove</td>
-        `;
+        `
         tbody.insertBefore(tr, tbody.lastElementChild)
         updateTotal()
     }
 
-    function updateTotal(){}
+    function rmCartElm(){
+        this.parentElement.remove()
+        updateTotal()
+    }
+
+    function quantityChange(){
+        if(e.target.value == '' || e.target.value.startWith('-') || e.target.value == '0')
+        e.target.value == '1'
+        updateTotal()
+    }
+
+    function updateTotal(){
+        total = 0
+        const rows = document.querySelectorAll('.item-row')
+        rows.forEach(row=>{
+            const ary = Array.from(row.children)
+            const elm = ary.find(x => x.firstElementChild instanceof HTMLInputElement)
+            let quantity = parseInt(elm.firstElementChild.value)
+            let price = (ary[2].innerText).replace('$', '')
+            price = price.replace( new RegExp('/.*'), '')
+            price = parseFloat(price)
+            total += quantity*price
+            total = Math.round(total*100)/100
+        })
+        const totalRow = document.querySelector('.totalRow')
+        totalRow.innerText = `${total}`
+    }
 
 })
